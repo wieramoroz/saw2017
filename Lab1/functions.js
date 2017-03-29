bb218d19b3d13d9bcd93fe3d6912dab9cae2324a
FabriSilve <fabrizio.silvestri@yahoo.it>
2017-03-29 17:32:31 +0200

    change lab1
    try to improve error message in "registrazione.html" end items layout

--- Lab1/functions.js
+++ Lab1/functions.js
@@ -1,22 +1,24 @@
 
 
 function validate_reg(form) {
-	var reason = "";
+	var error = false;
+	document.getElementById("user_err").innerText="";
+	document.getElementById("mail_err").innerText="";
+	document.getElementById("pass_err").innerText=""
 
-    if (form.username.value == "" || /[^a-zA-z]/.test(form.username.value))
-        reason += "Errore: il nome deve contenere solo le lettere\n";
-    if(form.mail.value=="" || /[^[a-z0-9_]+@[a-z0-9\-]+\.[a-z0-9\-\.]+$]]/.test(form.mail.value))
-    	reason += "Errore: la mail non e' valida\n";
-    if (form.password.value == "" || /[^0-9]/.test(form.password.value))
-        reason += "Errore: la password deve contenere solo i numeri\n";
-    
-
-    if (reason == "")
-        return true;  
-    else {
-        alert(reason);  
-        return false;
+    if (form.username.value == "" || /[^a-zA-z]/.test(form.username.value)) {
+    	error= true;
+        document.getElementById("user_err").innerText="Deve contenere solo lettere";
     }
+    if(form.mail.value=="" || /[^[a-z0-9_]+@[a-z0-9\-]+\.[a-z0-9\-\.]+$]]/.test(form.mail.value)) {
+    	error = true;
+    	document.getElementById("mail_err").innerText = "La mail non e' valida";
+    }
+    if (form.password.value == "" || /[^0-9]/.test(form.password.value)) {
+    	error = true;
+        document.getElementById("pass_err").innerText = "Deve contenere solo numeri";
+    }
+    return !error;
 }
 
 function validate_log(form) {
@@ -35,3 +37,11 @@ function validate_log(form) {
         return false;
     }
 }
+
+/*
+========= 29/3/17 - Fabrizio
+modificato le funzioni perche in caso di errore non stampino su un aletr
+ma su un label accanto al campo errato
+========= 
+
+*/
